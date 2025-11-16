a4920b20c1cd9518197b2fb5413ae5530f44c27a
Vitor Dutra Melo <vitordutra1125@gmail.com>
2025-11-16 19:49:47 +0000

    refactor: clean up controller code

--- bakend/consulta-cep/src/main/java/br/com/vitorMelo/consulta_cep/SpringCepApplication.java
+++ bakend/consulta-cep/src/main/java/br/com/vitorMelo/consulta_cep/SpringCepApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsultaCepApplication {
+public class SpringCepApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsultaCepApplication.class, args);
+		SpringApplication.run(SpringCepApplication.class, args);
 	}
 
 }
--- bakend/consulta-cep/src/main/java/br/com/vitorMelo/consulta_cep/controller/ConsultaController.java
+++ bakend/consulta-cep/src/main/java/br/com/vitorMelo/consulta_cep/controller/ConsultaController.java
@@ -1,16 +1,29 @@
 package br.com.vitorMelo.consulta_cep.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.vitorMelo.consulta_cep.domain.CepDomain;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/cep")
+@CrossOrigin(origins = "http://localhost:3000") // <-- Aqui você libera o React
 public class ConsultaController {
 
     // Requisicao do tipo get
-    @GetMapping("/boasvindas")
-    public String boasVindas() {
-        return "Seja bem-vindo ao sistema de consulta de CEP!";
+    // http://localhost:8080/api/cep/{cep}
+    @GetMapping("/{cep}")
+    public CepDomain consultaCep(@PathVariable("cep") String cep) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<CepDomain> resp = restTemplate.getForEntity(
+           String.format("https://viacep.com.br/ws/%s/json/", cep),
+           CepDomain.class
+        );
+        return resp.getBody();
     }
 }
